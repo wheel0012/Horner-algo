2f191859f364a0b5b62abd50106bac749f74c20e
wheel0012 <wheel0012@outlook.kr>
2019-07-15 11:51:56 +0900

    asd\

---
+++ BinarySearch.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+class Search
+{
+    public:
+    int *arr;
+    int BinarySearch(int* arr, int value ,int left, int right)
+    {
+        int middle = (left + right)/2;
+        if(arr[middle] > value) return BinarySearch(arr, value, left, middle);
+        else if(arr[middle]<value)return BinarySearch(arr, value, middle, right);
+        else if(arr[middle] == value)return middle;
+         
+        return -1;
+    }/*
+    private:
+    int Size(int* arr)
+    {
+        int i = 0; 
+        while(i++)
+        {
+            if(arr[i])break;
+        }
+        return i;
+    }*/
+};
+int main()
+{
+    Search bs; 
+    //char *str = (char *)malloc(sizeof(char) * 200);
+    //std::cin >> str;
+    //fflush(stdin);
+    int arr[10] = {1, 4, 5, 7, 8, 9, 10, 10, 11, 12};
+    int result = bs.BinarySearch(arr, 11, 0, 9);
+    std::cout << result << std::endl;
+    getchar();
+    return 0;
+}  
