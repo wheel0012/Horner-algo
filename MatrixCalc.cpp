335da30b7bf63c3214b7a6b48ab288359ea2a263
wheel0012 <wheel0012@outlook.kr>
2019-07-27 01:02:07 +0900

    asd

--- MatrixCalc.cpp
+++ MatrixCalc.cpp
@@ -26,7 +26,7 @@ class Matrix
     Matrix  operator+(const Matrix& b)
     {
         Matrix *temp = new Matrix(this->cLength, this->rLength);
-        int cLength = abs(this->cLength - b.cLength);
+        int cLength = abs(this->cLength - b.cLength);0
         int rLength = abs(this->rLength - b.rLength);
         for(int i = 0; i < cLength; i++)
             for(int j = 0; j<rLength; j++)
@@ -49,12 +49,12 @@ int main()
 {
     srand((unsigned int)time(0));
     std::cout<<"Matrix 1"<<std::endl;
-    //Matrix *matrix1= new Matrix(2, 3);
-    //matrix1->PrintMatrix();
+    Matrix *matrix1= new Matrix(2, 3);
+    matrix1->PrintMatrix();
     //free(matrix1);
     std::cout<<"Matrix 2"<<std::endl;
-    Matrix *matrix2 = new Matrix(3,2);
-    matrix2->PrintMatrix();
+    //Matrix *matrix2 = new Matrix(2,3);
+    //matrix2->PrintMatrix();
     std::cout<<"step3"<<std::endl;
     //Matrix result = *matrix1 + *matrix;
     std::cout<<"step4"<<std::endl;
