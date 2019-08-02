07924864d200108982d0e68a92c7bd2c5da16727
wheel0012 <wheel0012@outlook.kr>
2019-08-02 21:30:55 +0900

    Repair plus and minus operations

--- MatrixCalc.cpp
+++ MatrixCalc.cpp
@@ -19,7 +19,6 @@ class Matrix
     }
     Matrix()
     {
-        std::cout<<"I'm Mat 2"<<std::endl;
     }
     void InitialRandom()
     {
@@ -31,12 +30,32 @@ class Matrix
     {
         int cLength = this->cLength > b.cLength ? b.cLength : this->cLength;
         int rLength = this->rLength > b.rLength ? b.rLength : this->rLength;
-        Matrix *temp = new Matrix(cLength, rLength);
+        Matrix *temp = new Matrix(this->cLength, this->rLength);
+        for(int i = 0; i < this->cLength; i++)
+            for(int j = 0; j<this->rLength; j++)
+                temp->matrix[i][j] = this->matrix[i][j];
         for(int i = 0; i < cLength; i++)
             for(int j = 0; j<rLength; j++)
                 temp->matrix[i][j] = this->matrix[i][j]+ b.matrix[i][j];
         return *temp;
     };
+    Matrix& operator-(const Matrix& b)
+    {
+        int cLength = this->cLength > b.cLength ? b.cLength : this->cLength;
+        int rLength = this->rLength > b.rLength ? b.rLength : this->rLength;
+        Matrix *temp = new Matrix(this->cLength, this->rLength);
+        for(int i = 0; i < this->cLength; i++)
+            for(int j = 0; j<this->rLength; j++)
+                temp->matrix[i][j] = this->matrix[i][j];
+        for(int i = 0; i < cLength; i++)
+            for(int j = 0; j<rLength; j++)
+                temp->matrix[i][j] = this->matrix[i][j]- b.matrix[i][j];
+        return *temp;
+    }
+    Matrix& operator*(const Matrix& b)
+    {
+        
+    }
     void PrintMatrix()
     {
         for(int i = 0; i< this->rLength; i++)
@@ -53,17 +72,21 @@ int main()
 {
     srand((unsigned int)time(0));
     std::cout<<"Matrix 1"<<std::endl;
-    Matrix* matrix1= new Matrix(2, 3);
+    Matrix* matrix1= new Matrix(20, 30);
     matrix1->InitialRandom();
     matrix1->PrintMatrix(); 
     //delete matrix1;
     //free(matrix1);
     std::cout<<"Matrix 2"<<std::endl;
-    Matrix *m2 = new Matrix(3, 2);
+    Matrix *m2 = new Matrix(30, 20);
     m2->InitialRandom();
     m2->PrintMatrix();
     std::cout<<"Results"<<std::endl;
+    std::cout<<"Plus"<<std::endl;
     Matrix result = *matrix1 + *m2;
+    result.PrintMatrix();
+    std::cout<<"Minus"<<std::endl;
+    result = *matrix1 - *m2;
     result.PrintMatrix();
     std::cout<<"\n\nDone!"<<std::endl;
 }
