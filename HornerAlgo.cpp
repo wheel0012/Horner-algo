8f768a025a96378d9ad01b6f19487404e582e8a9
wheel0012 <wheel0012@outlook.kr>
2019-08-06 23:56:51 +0900

    add horner obj

--- HornerAlgo.cpp
+++ HornerAlgo.cpp
@@ -1,16 +1,59 @@
 #include <iostream>
-struct Cell{
+struct Cell
+{
     int coeficient;
     int indices;
-    int variable;
+    char variable;
+};
+class Horner
+{
+    Cell *cell;
+
+public:
+    Horner(Cell arr[])
+    {
+        this->cell = arr;
+    }
+    void Add(Cell single)
+    {
+
+        this->cell[length + 1] = single;
+        this->length++;
+    }
+
+    Cell* Calc()
+    {
+        
+        this->GetLength();
+    }
+    int GetLength()
+    {return this->length;}
+private:
+    int length;
 };
 int main()
 {
     struct Cell cell[50];
-
-    
-}
-Cell Calc(Cell arr[])
-{
+    for (int i = 0; i < 50; i++)
+    {
+        int temp = 0;
+        std::cout << "Insert params for expression. If you insert 0, insertion ends\ncoeficieient: " << std::ends;
+        std::cin >> temp;
+        if (temp == 0)
+            break;
+        else
+            cell[i].coeficient = temp;
+        std::cout << "Indices: " << std::ends;
+        std::cin >> temp;
+        if (temp == 0)
+            break;
+        else
+            cell[i].coeficient = temp;
+        std::cout << "variable: " << std::ends;
+        std::cin >> cell[i].variable;
+        system("cls");
+    }
+    Horner horner(cell);
+    horner.Calc();
 
 }
