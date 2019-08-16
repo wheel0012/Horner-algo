a285c04f97f38cc221135348bcd7f0a6d5923d3f
wheel0012 <wheel0012@outlook.kr>
2019-08-16 23:18:18 +0900

    QS

--- HornerAlgo.cpp
+++ HornerAlgo.cpp
@@ -10,20 +10,24 @@ class Horner
     Cell *cell;
 
 public:
-    Horner(Cell arr[])
+    Horner(Cell arr[], int length)
     {
         this->cell = arr;
+        this->length = length;
     }
     void Add(Cell single)
     {
-
         this->cell[length + 1] = single;
         this->length++;
     }
-
     Cell* Calc()
     {
-        this->GetLength();
+        int length = this->GetLength();
+        for(int i = 0; i< length; i++)
+        {
+            if(this->cell[i].variable)
+            std::cout<<this->cell[i].variable<<"||"<<this->cell[i].indices<<"||"<<this->cell[i].coeficient<<std::endl;
+        }
     }
     int GetLength()
     {return this->length;}
@@ -32,5 +36,23 @@ private:
 };
 int main()
 {
-    
+    int length = 0;
+    int maxLength = 50;
+    Cell asd[maxLength];
+    for(int i  = 0; i< maxLength; i++)
+    {
+        char temp;
+        std::cout<<"put variable"<<std::endl;
+        std::cin>>temp;
+        if(temp !='-')asd[i].variable = temp;
+        else break;
+        std::cout<<"put indices"<<std::endl;
+        std::cin>>asd[i].indices;
+        std::cout<<"put coefficient"<<std::endl;
+        std::cin>>asd[i].coeficient;
+        system("cls");
+        length++;
+    }
+    Horner horner(asd, length);
+    horner.Calc();
 }
--- QuickSort.cpp
+++ QuickSort.cpp
@@ -6,7 +6,7 @@ class QuickSort
     QuickSort()
     {
     }
-    T* Sort(T* arr, length)
+    T* Sort(T* arr, int length)
     {
         T* left;
         T* right;
@@ -18,13 +18,37 @@ class QuickSort
     int length;
     
 };
+/*
 template<typename T>
 class List
 {
-    public:
-    List()
+    /*
+    void Add(T val)
     {
-        
+        Node* temp = new Node<T>();
+        temp->Value = val;
+        temp->prevNode = this;
+        this->nextNode = temp;
+    }
+    void Del(int index)
+    {
+        for(int i = 0; i< index; i++)
+        {
+
+        }
+    }
+    static void RemoveAt(int index)
+    {
+    }
+    private:
+    int index;
+    List(int size, T initialValue)
+    {
+        Node<T> *temp = new Node<T>(initialValue)
+        for(int i = 0; i< size; i++)
+        {
+            Node<T> *temp = new Node<T>()
+        }
     }
     int Length()
     {
@@ -32,9 +56,59 @@ class List
     }
     private:
     int length;
+    
+};*/
+template<typename T>
+class Node
+{
+    private:
+    Node* prevNode;
+    Node* nextNode;
+    public:
+    T Value;
+    Node(Node *prev, T value)
+    {
+        this->prevNode = prev;
+        this->Value = value;
+        prev->nextNode = this;
+    }
+    Node(T value)
+    {
+        this->Value = value;
+    }
+    //Add node between two nodes
+    Node(Node *prev, T value, Node* next)
+    {
+        //value inital
+        this->Value = value;
+        //Setting prev node;
+        this->prevNode = prev;
+        prev->nextNode = this;
+        //Setting next node;
+        this->nextNode = next;
+        next->prevNode = this;
+    }
+    Node<T>* Next()
+    {
+        return nextNode;
+    }
+    Node<T>* Prev()
+    {
+        if(prevNode!=NULL)
+            return prevNode;
+        else return nullptr;
+    }
 };
-
 int main()
 {
     int arr[10] = {1,5,2,9,4,6,2,4,3,7};
+    Node<int>* node1 = new Node<int>(5);
+    Node<int>* node2 = new Node<int>(node1, 10);
+    std::cout<<node1->Value<<std::endl;
+    std::cout<<node1->Next()->Value<<std::endl;
+    std::cout<<"--------------"<<std::endl;
+    Node<int>* node3 = new Node<int>(node1, 7, node2);
+    std::cout<<node3->Prev()->Value<<std::endl;
+    std::cout<<node3->Value<<std::endl;
+    std::cout<<node3->Next()->Value<<std::endl;
 }
